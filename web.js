f238c42ce7857e9131854f93e4a32b380fc6c1b8
Carlin Eng <carlineng@gmail.com>
2013-11-30 01:50:31 +0000

    Add index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs')
 var app = express();
+
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  index_buffer = fs.readFileSync('index.html')
+  response.send(index_buffer.toString('utf-8'))
 });
 
 var port = process.env.PORT || 5000;
